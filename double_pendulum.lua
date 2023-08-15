a67487e3fd6cd25eb7a9936b68c3e988ac3e3b77
jaseknighter <52048666+jaseknighter@users.noreply.github.com>
2023-08-15 07:09:15 -0700

    musical pendulum
    two pendulums, make pendulums strikers, simplify code

--- double_pendulum.lua
+++ double_pendulum.lua
@@ -4,7 +4,29 @@
 --
 -- double pendulum code based on code from Daniel Shiffman's The Nature of Code (https://natureofcode.com/book/chapter-2-forces/).
 
-include "lib/includes"
+s=require("sequins")
+lattice=require("lattice")
+
+encoders_and_keys = include "double_pendulum/lib/encoders_and_keys"
+swing = include("double_pendulum/lib/swing")
+
+engine.name = "PolyPerc"
+
+-------------------------------------------
+-- global variables
+-------------------------------------------
+updating_controls = false
+OUTPUT_DEFAULT = 4
+SCREEN_FRAMERATE = 1/15
+menu_status = false
+
+alt_key_active = false
+screen_level_graphics = 15
+num_pages = 1
+
+initializing = true
+
+
 
 ------------------------------
 -- init
@@ -12,15 +34,16 @@ include "lib/includes"
 function init()
 
   -- set sensitivity of the encoders
-  norns.enc.sens(1,6)
-  norns.enc.sens(2,6)
-  norns.enc.sens(3,6)
-
-  pages = UI.Pages.new(0, 5)
     
+  sw1 = swing:new(40)
+  sw2 = swing:new(80,2.1*(math.pi)/2)
+  init_lattice()
+  lat:start()
+
   set_redraw_timer()
-  page_scroll(1)
   
+  engine.amp(1)
+  engine.release(1)
   initializing = false
 end
 
@@ -42,10 +65,129 @@ function set_redraw_timer()
   redrawtimer = metro.init(function() 
     local menu_status = norns.menu.status()
     if menu_status == false and initializing == false then
-      dpendulum_pages.update_pages()
+      screen.aa(0)
+      screen.clear()
+      screen.level(screen_level_graphics)
+      sw1.display()
+      sw2.display()
+      screen.update()
+
     end
   end, SCREEN_FRAMERATE, -1)
   redrawtimer:start()  
+end
+
+
+--------------------------
+-- functions 
+--------------------------
+-- overlap checker
+-- check if any
+-- point overlaps the given circle
+-- and rectangle
+-- from: https://www.geeksforgeeks.org/check-if-any-point-overlaps-the-given-circle-and-rectangle/#
+ 
+-- Function to check if any point
+-- overlaps the given circle
+-- and rectangle
+function checkOverlap(R, Xc, Yc, X1, Y1, X2, Y2)
+
+   
+    -- Find the nearest point on the
+    -- rectangle to the center of
+    -- the circle
+    local Xn = math.max(X1, math.min(Xc, X2))
+    local Yn = math.max(Y1, math.min(Yc, Y2))
+       
+    -- Find the distance between the
+    -- nearest point and the center
+    -- of the circle
+    -- Distance between 2 points,
+    -- (x1, y1) & (x2, y2) in
+    -- 2D Euclidean space is
+    -- ((x1-x2)**2 + (y1-y2)**2)**0.5
+    local Dx = Xn - Xc
+    local Dy = Yn - Yc
+    return (Dx * Dx + Dy * Dy) <= R * R
+end
+
+--[[     
+-- Driver Code
+local R = 1
+local Xc = 0, Yc = 0
+local X1 = 1, Y1 = -1
+local X2 = 3, Y2 = 1
+   
+if(checkOverlap(R, Xc, Yc,
+                   X1, Y1,
+                   X2, Y2))
+then
+    document.write("True" + "\n")
+else
+
+    document.write("False")
+end
+]]
+--[[
+engine.amp(1);engine.release(1)
+engine.hz(midi_to_hz(40))
+]]
+
+function midi_to_hz(note)
+  local hz = (440 / 32) * (2 ^ ((note - 9) / 12))
+  return hz
+end
+
+-- setup lattice and sequins
+
+-- mseq=s{40,42,44}
+mseq=s{s{40,30,70},s{42,46,73},s{52,78,58}}
+rseq=s{1/16}
+function init_lattice()
+  lat = lattice:new{
+    auto = true,
+    meter = 4,
+    ppqn = 96
+  }
+
+  -- setup a pattern to sequence softcut's filter frequency and rq
+  lat_pat1 = lat:new_sprocket{
+    action = function(t) 
+      sw1.update()
+      sw2.update()
+      local mel = mseq()
+      local rhy = rseq()
+      -- engine.hz(midi_to_hz(mel))
+      -- lat_pat1:set_division(rhy)
+    end,
+    division = 1/64,
+    enabled = true
+  }
+  
+  -- setup a pattern to sequence softcut's filter frequency and rq
+  lat_pat2 = lat:new_sprocket{
+    action = function(t) 
+      
+      if #sw1.theta1_tb > 0 then
+        local t2min = math.min(table.unpack(sw1.theta2_tb))
+        local t2max = math.max(table.unpack(sw1.theta2_tb))
+        local cut = util.linlin(t2min,t2max,100,10000,sw1.theta2)
+        local pw = util.linlin(t2min,t2max,0.1,0.5,sw1.theta2)
+
+        -- print(sw1.my_circle1.x-40, sw1.my_circle2.x-84,cut_val)
+        local circle_distance = (sw1.my_circle1.x-40) + (sw1.my_circle2.x - 40)
+        local cut = util.linlin(-50,50,100,10000,circle_distance)
+        local pw = util.linlin(-50,50,0.1,0.5,circle_distance)
+        -- print(cut,pw)
+        -- local cut = util.linlin(t2min,t2max,100,10000,sw1.theta2)
+        -- local pw = util.linlin(t2min,t2max,0.1,0.5,sw1.theta2)
+        engine.cutoff(cut)
+        engine.pw(pw)
+      end
+    end,
+    division = 1/128,
+    enabled = true
+  }
 end
 
 
--- lib/swing.lua
+++ lib/swing.lua
@@ -1,91 +1,133 @@
 -- From: https://github.com/micaeloliveira/physics-sandbox/blob/feature/new-styling/assets/javascripts/pendulum.js
 
-local d2_theta1 = 0
-local d2_theta2 = 0
-local d_theta1 = 0
-local d_theta2 = 0
-local theta1 = 0*(math.pi)/2
-local theta2 = 2.3*(math.pi)/2
-local m1 = 5
-local m2 = 3
-local l1 = 150/6
-local l2 = 150/6
-local x0 = 350/6
-local y0 = 60/6
-local g = 9.8
-local time = 0.2
+local swing = {}
+swing.__index = swing
 
-local my_line1 = {}
-my_line1.x0 = x0
-my_line1.y0 = y0
-my_line1.x = 0
-my_line1.y = 0
+function swing:new(x,theta2)
+  local sw = {}
+  setmetatable(sw,swing)
+  sw.d2_theta1 = 0
+  sw.d2_theta2 = 0
+  sw.d_theta1 = 0
+  sw.d_theta2 = 0
+  sw.theta1 = 0.1 -- 0*(math.pi)/2
+  sw.theta2 = theta2 or 2.3*(math.pi)/2
+  sw.m1 = 7
+  sw.m2 = 7
+  sw.l1 = 25 --150/6
+  sw.l2 = 25 --150/6
+  sw.x0 = x or 58.333 --350/6
+  sw.y0 = 10 --60/6
+  sw.g = 9.8
+  sw.time = 0.2
 
-local my_line2 = {}
-my_line2.x0 = 0
-my_line2.y0 = 0
-my_line2.x = 0
-my_line2.y = 0
-
-my_circle1 = {}
-my_circle1.x = x0+l1*math.sin(theta1)
-my_circle1.y = y0+l1*math.cos(theta1)
-my_circle1.mass = m1
-
-local my_circle2 = {}
-my_circle2.x = x0+l1*math.sin(theta1)+l2*math.sin(theta2)
-my_circle2.y = y0+l1*math.cos(theta1)+l2*math.cos(theta2)
-my_circle2.mass = m2
-
-----
--- define function to run with each redraw cycle
-----
-update = function ()
-  local update_fn = function()
-    mu = 1+m1/m2
-    d2_theta1 = (g*(math.sin(theta2)*math.cos(theta1-theta2)-mu*math.sin(theta1))-(l2*d_theta2*d_theta2+l1*d_theta1*d_theta1*math.cos(theta1-theta2))*math.sin(theta1-theta2))/(l1*(mu-math.cos(theta1-theta2)*math.cos(theta1-theta2)))
-    d2_theta2 = (mu*g*(math.sin(theta1)*math.cos(theta1-theta2)-math.sin(theta2))+(mu*l1*d_theta1*d_theta1+l2*d_theta2*d_theta2*math.cos(theta1-theta2))*math.sin(theta1-theta2))/(l2*(mu-math.cos(theta1-theta2)*math.cos(theta1-theta2)))
-    d_theta1 = d_theta1 + d2_theta1*time
-    d_theta2 = d_theta2 + d2_theta2*time
-    theta1 = theta1 + d_theta1*time
-    theta2 = theta2 + d_theta2*time
+  sw.my_line1 = {}
+  sw.my_line1.x0 = sw.x0
+  sw.my_line1.y0 = sw.y0
+  sw.my_line1.x = 0
+  sw.my_line1.y = 0
   
-    my_circle1.x = x0+l1*math.sin(theta1)
-    my_circle1.y = y0+l1*math.cos(theta1)
-    my_circle2.x = x0+l1*math.sin(theta1)+l2*math.sin(theta2)
-    my_circle2.y = y0+l1*math.cos(theta1)+l2*math.cos(theta2)
+  sw.my_line2 = {}
+  sw.my_line2.x0 = 0
+  sw.my_line2.y0 = 0
+  sw.my_line2.x = 0
+  sw.my_line2.y = 0
   
-    my_line1.x  = my_circle1.x
-    my_line1.y  = my_circle1.y
-    my_line2.x0 = my_circle1.x
-    my_line2.y0 = my_circle1.y
-    my_line2.x  = my_circle2.x
-    my_line2.y  = my_circle2.y
+  sw.my_circle1 = {}
+  sw.my_circle1.x = sw.x0+sw.l1*math.sin(sw.theta1)
+  sw.my_circle1.y = sw.y0+sw.l1*math.cos(sw.theta1)
+  sw.my_circle1.mass = sw.m1
+  sw.my_circle1.radius = sw.m1*0.5
+
+  sw.my_circle2 = {}
+  sw.my_circle2.x = sw.x0+sw.l1*math.sin(sw.theta1)+sw.l2*math.sin(sw.theta2)
+  sw.my_circle2.y = sw.y0+sw.l1*math.cos(sw.theta1)+sw.l2*math.cos(sw.theta2)
+  sw.my_circle2.mass = sw.m2
+  sw.my_circle2.radius = sw.m2*0.5
+  
+  sw.theta1_tb = {}
+  sw.theta2_tb = {}
+  -- math.max(table.unpack(sw1.theta1_tb))
+  -- math.min(table.unpack(sw1.theta2_tb))
+  -- math.min(table.unpack(sw2.theta1_tb))
+  -- math.min(table.unpack(sw2.theta2_tb))
+  ----
+  -- define function to run with each redraw cycle
+  ----
+  sw.update = function ()
+    sw.mu = 1+sw.m1/sw.m2
+    sw.d2_theta1 = (sw.g*(math.sin(sw.theta2)*math.cos(sw.theta1-sw.theta2)-sw.mu*math.sin(sw.theta1))-(sw.l2*sw.d_theta2*sw.d_theta2+sw.l1*sw.d_theta1*sw.d_theta1*math.cos(sw.theta1-sw.theta2))*math.sin(sw.theta1-sw.theta2))/(sw.l1*(sw.mu-math.cos(sw.theta1-sw.theta2)*math.cos(sw.theta1-sw.theta2)))
+    sw.d2_theta2 = (sw.mu*sw.g*(math.sin(sw.theta1)*math.cos(sw.theta1-sw.theta2)-math.sin(sw.theta2))+(sw.mu*sw.l1*sw.d_theta1*sw.d_theta1+sw.l2*sw.d_theta2*sw.d_theta2*math.cos(sw.theta1-sw.theta2))*math.sin(sw.theta1-sw.theta2))/(sw.l2*(sw.mu-math.cos(sw.theta1-sw.theta2)*math.cos(sw.theta1-sw.theta2)))
+    sw.d_theta1 = sw.d_theta1 + sw.d2_theta1*sw.time
+    sw.d_theta2 = sw.d_theta2 + sw.d2_theta2*sw.time
+    sw.theta1 = sw.theta1 + sw.d_theta1*sw.time
+    sw.theta2 = sw.theta2 + sw.d_theta2*sw.time
+  
+    sw.my_circle1.x = sw.x0+sw.l1*math.sin(sw.theta1)
+    sw.my_circle1.y = sw.y0+sw.l1*math.cos(sw.theta1)
+    sw.my_circle2.x = sw.x0+sw.l1*math.sin(sw.theta1)+sw.l2*math.sin(sw.theta2)
+    sw.my_circle2.y = sw.y0+sw.l1*math.cos(sw.theta1)+sw.l2*math.cos(sw.theta2)
+  
+    sw.my_line1.x  = sw.my_circle1.x
+    sw.my_line1.y  = sw.my_circle1.y
+    sw.my_line2.x0 = sw.my_circle1.x
+    sw.my_line2.y0 = sw.my_circle1.y
+    sw.my_line2.x  = sw.my_circle2.x
+    sw.my_line2.y  = sw.my_circle2.y
+    table.insert(sw.theta1_tb,sw.d_theta1)
+    table.insert(sw.theta2_tb,sw.d_theta2)
   end
-  update_fn()
-  
-  local display_fn = function()
-    -- screen.level(5)
-    screen.aa(1)
 
-    screen.clear()
-    screen.level(screen_level_graphics)
+sw.display = function()
+      local t2min = math.min(table.unpack(sw.theta2_tb))
+      local t2max = math.max(table.unpack(sw.theta2_tb))
 
-    screen.move(my_line1.x0,my_line1.y0)
-    screen.line(my_line1.x,my_line1.y)
+      -- local lvl = util.linlin(t2min,t2max,3,15,sw.theta2)
+      local circle_distance = (sw.my_circle1.x-40) + (sw.my_circle2.x - 40)
+      local lvl = util.linlin(-50,50,3,15,circle_distance)
+
+    -- print(lvl,t2min,t2max,3,15,sw.theta2)
+    screen.level(math.floor(lvl))
+
+    screen.move(sw.my_line1.x0,sw.my_line1.y0)
+    screen.line(sw.my_line1.x,sw.my_line1.y)
     screen.stroke()
-    screen.move(my_line2.x0,my_line2.y0)
-    screen.line(my_line2.x,my_line2.y)
+    screen.move(sw.my_line2.x0,sw.my_line2.y0)
+    screen.line(sw.my_line2.x,sw.my_line2.y)
     screen.stroke()
-    screen.circle(my_circle1.x,my_circle1.y,my_circle1.mass)
-    screen.circle(my_circle2.x,my_circle2.y,my_circle2.mass)
+    screen.circle(sw.my_circle1.x,sw.my_circle1.y,sw.my_circle1.radius)
+    screen.circle(sw.my_circle2.x,sw.my_circle2.y,sw.my_circle2.radius)
     screen.fill()
     
-    screen.update()
+    local R = sw.my_circle2.radius
+    local Xc = sw.my_line2.x
+    local Yc = sw.my_line2.y
+    local X1 = 25
+    local Y1 = 50
+    local X2 = X1+10
+    local Y2 = Y1+10
+    --draw a recr
+    
+    screen.move (X1,Y1)
+    screen.rect(X1,Y1, 10, 10)
+    -- print("checkOverlap",checkOverlap(R, Xc, Yc,X1, Y1,X2, Y2))
+    check_strike(R, Xc, Yc,X1, Y1,X2, Y2)
+    screen.stroke()
+    -- screen.update()
   end
-  display_fn()
+  
+  function check_strike(R, Xc, Yc,X1, Y1,X2, Y2)
+      local overlap = checkOverlap(R, Xc, Yc,X1, Y1,X2, Y2)
+      if overlap then
+        local mel = mseq()
+        engine.hz(midi_to_hz(mel))  
+      end
+    end
+  -- return {
+  --   update = update
+  -- }
+  
+  return sw
 end
 
-return {
-  update = update
-}
+return swing
